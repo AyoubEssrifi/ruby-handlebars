4703703372fac4d8447ebfdc12e9cc99020365e9
AyoubEssrifi <essrifiayoub@gmail.com>
2023-10-27 16:18:16 +0200

    Support hash iterating with each helper

--- lib/ruby-handlebars/helpers/each_helper.rb
+++ lib/ruby-handlebars/helpers/each_helper.rb
@@ -19,7 +19,14 @@ module Handlebars
         else
           context.with_temporary_context(name => nil, :@index => 0, :@first => false, :@last => false) do
             result = items.each_with_index.map do |item, index|
-              context.add_items(name => item, :@index => index, :@first => (index == 0), :@last => (index == items.length - 1))
+              # Case 1: loop through a Hash
+              if item.is_a?(Array) && item.length == 2 && item[0].is_a?(Symbol)
+                context.add_items(name => item[1], :@key=> item[0], :@index => index, :@first => (index == 0), :@last => (index == items.length - 1))
+              # Case 2: we loop through an Array
+              else
+                context.add_items(name => item, :@index => index, :@first => (index == 0), :@last => (index == items.length - 1))
+              end
+
               block.fn(context)
             end.join('')
           end
